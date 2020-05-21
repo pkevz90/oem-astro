098bb0a0f4c90d2fa4aab0d7b947ebd6e017e557
pkevz90 <65626812+pkevz90@users.noreply.github.com>
2020-05-20 19:28:22 -0700

    created multiple player for waypoint planner

---
+++ SupportLibraries/WaypointPlannerJSMulti/WPmainModule.js
@@ -0,0 +1,303 @@
+let globalChartRef;
+let tooltipOpen, 
+	tooltipIndex, 
+	chosenWaypoint,
+	dragPoint, 
+	axisLimits = 20, 
+	tacticArray = [], passiveTime = 12, 
+	tactic = undefined, 
+	idInterval, 
+	wholeTraj, 
+	playFrame, playBool = false, 
+	initSunVector = [[axisLimits*3/8],[0]], playDt = 100, numSensors = 0,
+	startTime,
+	maneuverListSpans,
+	firstClick = undefined,
+	dataPoints = {
+		workingRso: 1,
+		chosenWaypoint: 4,
+		techniquePlot: 5,
+		passiveTrajectory: 6,
+		sun: 7,
+		moon: 8,
+		burnDirections: 9,
+		sensorStart: 10
+	};
+// console.log(math.inv([[2,-1,3],[2,-2,1],[1,1,1]]));
+function createGraph() {
+    var config = {
+        type: 'scatter',
+        data: {
+            datasets: [{
+                // label: "BlueWaypoints",
+                data: [{
+					x: -15,
+					y: 0,
+					dx: 0,
+					dy: 0,
+					deltaX: 0,
+					deltaY: 0,
+					time: 0
+				}],
+                fill: false,
+                showLine: false,
+				pointRadius: 7,
+				borderColor: 'rgba(60,100,255,1)'
+            },{
+                // label: "Trajectory",
+                data: [],
+                fill: false,
+                showLine: true,
+				pointRadius: 0,
+				borderColor: 'rgba(60,100,255,1)'
+				// borderColor: 'rgba(255,100,60,1)'
+            },{
+                // label: "RedWaypoints",
+                data: [{
+					x: -15,
+					y: 0,
+					dx: 0,
+					dy: 0,
+					deltaX: 0,
+					deltaY: 0,
+					time: 0
+				}],
+                fill: false,
+                showLine: false,
+				pointRadius: 7,
+				borderColor: 'rgba(255,100,60,1)'
+            },{
+                // label: "Trajectory",
+                data: [],
+                fill: false,
+                showLine: true,
+				pointRadius: 0,
+				borderColor: 'rgba(255,100,60,1)'
+            },{
+                // label: "Chosen Point",
+                data: [],
+                fill: false,
+                showLine: false,
+				pointRadius: 8,
+				pointBackgroundColor: 'rgba(255,0,0,1)'
+            },{
+                // label: "Technique Plot",
+                data: [],
+                fill: false,
+                showLine: true,
+				pointRadius: 0,
+				borderDash: [10,10],
+				borderColor: 'rgba(255,255,255,1)'
+            },{
+                // label: "Passive Trajectory",
+                data: [],
+                fill: false,
+                showLine: true,
+				pointRadius: 0,
+				borderColor: 'rgba(120,200,250,0.5)'
+            },{
+                // label: "Sun",
+                data: [],
+                fill: false,
+                showLine: true,
+				pointRadius: 0,
+				borderWidth: 6,
+				borderColor: 'rgba(200,200,0,1)',
+            },{
+                // label: "Moon",
+                data: [],
+                fill: false,
+                showLine: true,
+				pointRadius: 0,
+				borderWidth: 6,
+				borderColor: 'rgba(150,150,150,1)',
+            },{
+                // label: "Burn Direction",
+                data: [{
+					x: 0,
+					y: 0,
+				},{
+					x: 0,
+					y: 0,
+				}],
+                fill: false,
+                showLine: true,
+				pointRadius: 0,
+				borderWidth: 3,
+				spanGaps: false,
+				borderColor: 'rgba(255,255,255,1)',
+            },{
+				// label: "Sensor",
+                data: [],
+                fill: true,
+                showLine: true,
+				pointRadius: 0,
+				lineTension: 0,
+				backgroundColor: 'rgba(255,150,150,0.5)'
+			}]
+        },
+        options: {
+			onResize: function (element,a) {
+				// console.log(globalChartRef.config.options.scales.xAxes[0].scaleLabel)
+				setLabelSize(a.width);
+            },
+			animation:{
+				duration: 0
+			},
+			tooltips: {
+				enabled: false
+			},
+			legend: {
+				display: false
+			},
+			onHover: function(element) {
+				let scaleRef,
+				valueX,
+				valueY;
+				for (var scaleKey in this.scales) {
+					scaleRef = this.scales[scaleKey];
+					if (scaleRef.isHorizontal() && scaleKey == 'x-axis-1') {
+						valueX = scaleRef.getValueForPixel(element.offsetX);
+					} else if (scaleKey == 'y-axis-1') {
+						valueY = scaleRef.getValueForPixel(element.offsetY);
+					}
+				}
+				handleHover(valueX,valueY);
+			},
+            onClick: function (element, dataAtClick) {
+				// console.log('click')
+                let scaleRef,
+                    valueX,
+                    valueY;
+                for (var scaleKey in this.scales) {
+                    scaleRef = this.scales[scaleKey];
+                    if (scaleRef.isHorizontal() && scaleKey == 'x-axis-1') {
+                        valueX = scaleRef.getValueForPixel(element.offsetX);
+                    } else if (scaleKey == 'y-axis-1') {
+                        valueY = scaleRef.getValueForPixel(element.offsetY);
+                    }
+				}
+				handleClick(valueX,valueY,element.shiftKey || element.ctrlKey);
+            },
+            title: {
+                display: false,
+                text: "",
+				fontColor: 'rgba(255,255,255,1)',
+				fontSize: 40
+            },
+            scales: {
+                xAxes: [{
+					gridLines: {
+						zeroLineColor: 'rgba(255,255,255,0.5)',
+						color: 'rgba(255,255,255,0.25)'
+					},
+                    type: "linear",
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'In-Track',
+						fontColor: 'rgba(255,255,255,1)',
+						fontSize: 20,
+                    },
+					ticks: {
+						min: -20,
+						max: 20,
+						fontSize: 20,
+						reverse: true,
+						fontColor: 'rgba(255,255,255,1)',
+					}
+                }, ],
+                yAxes: [{
+					gridLines: {
+						zeroLineColor: 'rgba(255,255,255,0.5)',
+						color: 'rgba(255,255,255,0.25)'
+					},
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Radial',
+						fontColor: 'rgba(255,255,255,1)',
+						fontSize:  20,
+                    },
+					ticks: {
+						min: -15,
+						max: 15,
+						fontColor: 'rgba(255,255,255,1)',
+						fontSize: 20,
+					}
+                }]
+            },
+            responsive: true,
+            maintainAspectRatio: true
+        }
+    };
+    
+    config.data.datasets.forEach(function (dataset) {
+        dataset.pointBorderWidth = 2;
+        dataset.pointHoverRadius = 12;
+    });
+    
+    var ctx = document.getElementById('ChartCnvs').getContext('2d');
+    globalChartRef = new Chart(ctx, config);
+}
+
+
+window.addEventListener('DOMContentLoaded', function () {
+	createGraph();
+	setLabelSize($('#ChartCnvs').width()); // Scale label size to browser window size
+	let v = document.getElementById("start-time").value;
+	startTime = [Number(v.substring(0,4)), Number(v.substring(5,7)), Number(v.substring(8,10)), Number(v.substring(11,13)), Number(v.substring(14,16))];
+	drawSunMoonVectors(julianDateCalc(startTime));
+	maneuverListSpans = document.getElementById("burnTable").querySelectorAll("span");
+	annotateBurnHistory();
+	globalChartRef.update();
+	document.getElementById("ChartCnvs").addEventListener("wheel", function(a){
+		a.preventDefault();
+		if (tactic !== undefined) {
+			handleTechnique(tactic,undefined,undefined,'wheel',a.deltaY);
+			return;
+		}
+		
+		let trajSet = (dataPoints.workingRso-1)*2;
+		if (a.deltaY > 0){
+			if (globalChartRef.config.data.datasets[trajSet].data[chosenWaypoint].time > 300){
+				globalChartRef.config.data.datasets[trajSet].data[chosenWaypoint].time -= 300;
+			}
+			else {
+				showNoteBar('Cannot have negative transfer time');
+			}
+		}
+		else{
+			globalChartRef.config.data.datasets[trajSet].data[chosenWaypoint].time += 300;
+		}
+		calculateTrajecories();
+		drawSunMoonVectors(julianDateCalc(startTime),Number(maneuverListSpans[chosenWaypoint*5].innerText)*3600);
+	});
+	document.addEventListener('keypress', function(key){
+		let k = key.key;
+		handleKeyPress(k);
+	});
+	// console.log(document.getElementById("Play"));
+	document.getElementById("start-time").addEventListener("change",(a)=>{
+		let v = a.srcElement.value;
+		startTime = [Number(v.substring(0,4)), Number(v.substring(5,7)), Number(v.substring(8,10)), Number(v.substring(11,13)), Number(v.substring(14,16))];
+    	drawSunMoonVectors(julianDateCalc(startTime));
+	});
+	document.getElementById("sunCheck").addEventListener("change",() => {
+		drawSunMoonVectors(julianDateCalc(startTime));
+	})
+	document.getElementById("moonCheck").addEventListener("change",() => {
+		drawSunMoonVectors(julianDateCalc(startTime));
+	})
+	document.getElementById("burnCheck").addEventListener("change",() => {
+		plotBurnDirections();
+		globalChartRef.update();
+	})
+}, false);
+
+function setLabelSize(canvasWidth) {
+	globalChartRef.config.options.scales.xAxes[0].scaleLabel.fontSize = canvasWidth/40;
+	globalChartRef.config.options.scales.yAxes[0].scaleLabel.fontSize = canvasWidth/40;
+	globalChartRef.config.options.scales.xAxes[0].ticks.fontSize = canvasWidth/60;
+	globalChartRef.config.options.scales.yAxes[0].ticks.fontSize = canvasWidth/60
+}
---
+++ SupportLibraries/WaypointPlannerJSMulti/WayTrajectories.js
@@ -0,0 +1,141 @@
+function calculateTrajecories(){
+	let trajSet = (dataPoints.workingRso-1)*2+1;
+	
+	let numWaypoints = globalChartRef.config.data.datasets[trajSet-1].data.length;
+	let r1, r2, r, v1,t;
+	var dt = 1000;
+	globalChartRef.config.data.datasets[trajSet].data = [];
+	let vxOld = 0, vyOld = 0; 
+	for (var ii = 0; ii < (numWaypoints-1); ii++){
+		console.log(trajSet,ii);
+		t = globalChartRef.config.data.datasets[trajSet-1].data[ii+1].time;
+		r1 = [[globalChartRef.config.data.datasets[trajSet-1].data[ii].y],[globalChartRef.config.data.datasets[trajSet-1].data[ii].x]];
+		r2 = [[globalChartRef.config.data.datasets[trajSet-1].data[ii+1].y],[globalChartRef.config.data.datasets[trajSet-1].data[ii+1].x]];
+
+		v1 = math.multiply(math.inv(PhiRV(t)),math.subtract(r2,math.multiply(PhiRR(t),r1)));
+		v2 = math.add(math.multiply(PhiVR(t),r1),math.multiply(PhiVV(t),v1));
+		// console.log(v2);
+		globalChartRef.config.data.datasets[trajSet-1].data[ii].deltaX = v1[1][0]-vxOld; 
+		globalChartRef.config.data.datasets[trajSet-1].data[ii].deltaY = v1[0][0]-vyOld;
+		// console.log(v1[1][0],v1[0][0])
+		// console.log(globalChartRef.config.data.datasets[0].data[ii+1].deltaX,globalChartRef.config.data.datasets[0].data[ii+1].deltaY)
+		globalChartRef.config.data.datasets[trajSet-1].data[ii+1].dy = vyOld; 
+		vxOld = v2[1][0]; vyOld = v2[0][0];
+		globalChartRef.config.data.datasets[trajSet-1].data[ii+1].dx = vxOld; 
+		globalChartRef.config.data.datasets[trajSet-1].data[ii+1].dy = vyOld; 
+		for (var kk = 0; kk < t; kk += dt){
+			r = math.add(math.multiply(PhiRR(kk),r1),math.multiply(PhiRV(kk),v1));
+			
+			globalChartRef.config.data.datasets[trajSet].data.push({
+				x: r[1],
+				y: r[0]
+			});
+		}
+		globalChartRef.config.data.datasets[trajSet].data.push({
+			x: r2[1],
+			y: r2[0]
+		});
+	}
+	if (chosenWaypoint !== undefined){
+		calcPassiveTraj();
+	}
+	plotBurnDirections();
+	annotateBurnHistory();
+	globalChartRef.update();
+}
+function playTrajectory(){
+	let numWaypoints = globalChartRef.config.data.datasets[0].data.length;
+	let r1, r2, r, v1,t, PRR, PRV;
+	var dt = 100;
+	globalChartRef.config.data.datasets[1].data = []; let traj = [];
+	for (var ii = 0; ii < (numWaypoints-1); ii++){
+		t = globalChartRef.config.data.datasets[0].data[ii+1].time;
+		r1 = [[globalChartRef.config.data.datasets[0].data[ii].y],[globalChartRef.config.data.datasets[0].data[ii].x]];
+		r2 = [[globalChartRef.config.data.datasets[0].data[ii+1].y],[globalChartRef.config.data.datasets[0].data[ii+1].x]];
+
+		v1 = math.multiply(math.inv(PhiRV(t)),math.subtract(r2,math.multiply(PhiRR(t),r1)));
+		for (var kk = 0; kk < t; kk += dt){
+			r = math.add(math.multiply(PhiRR(kk),r1),math.multiply(PhiRV(kk),v1));
+			traj.push([r[1],r[0]]);
+		}
+	}
+	return traj;
+}
+function calcPassiveTraj(){
+	// Show passive trajectory
+	let trajSet = (dataPoints.workingRso-1)*2;
+	globalChartRef.config.data.datasets[dataPoints.passiveTrajectory].data = [];
+	let rPass = [[globalChartRef.config.data.datasets[trajSet].data[chosenWaypoint].y],[globalChartRef.config.data.datasets[trajSet].data[chosenWaypoint].x]];
+	let vPass = [[globalChartRef.config.data.datasets[trajSet].data[chosenWaypoint].dy],[globalChartRef.config.data.datasets[trajSet].data[chosenWaypoint].dx]];
+	let r2;
+	for (var jj = 0; jj <= passiveTime; jj+= 0.5){
+		r2 = math.add(math.multiply(PhiRR(jj*3600),rPass),math.multiply(PhiRV(jj*3600),vPass));
+		globalChartRef.config.data.datasets[dataPoints.passiveTrajectory].data.push({
+			x: r2[1],
+			y: r2[0],
+		});
+	}
+}
+function plotBurnDirections(){
+	// console.log(index,'dit')
+	let trajSet = (dataPoints.workingRso-1)*2;
+	let numWaypoints = globalChartRef.config.data.datasets[trajSet].data.length;
+	globalChartRef.config.data.datasets[dataPoints.burnDirections].data = [];
+	if (!document.getElementById("burnCheck").checked) {
+		return;
+	}
+	for (var ii = 0; ii < (numWaypoints-1); ii++){
+		globalChartRef.config.data.datasets[dataPoints.burnDirections].data.push({
+			x: globalChartRef.config.data.datasets[trajSet].data[ii].x,
+			y: globalChartRef.config.data.datasets[trajSet].data[ii].y,
+		})
+		globalChartRef.config.data.datasets[dataPoints.burnDirections].data.push({
+			x: globalChartRef.config.data.datasets[trajSet].data[ii].x+globalChartRef.config.data.datasets[trajSet].data[ii].deltaX*4000,
+			y: globalChartRef.config.data.datasets[trajSet].data[ii].y+globalChartRef.config.data.datasets[trajSet].data[ii].deltaY*4000,
+		})
+		globalChartRef.config.data.datasets[dataPoints.burnDirections].data.push({
+			x: NaN,
+			y: NaN
+		})
+	}
+} 
+function drawSunMoonVectors(time,dt) {
+	let long = 0*Math.PI/180;
+	let theta0 = thetaGMST(time)*Math.PI/180;
+	let n = 2*Math.PI/86164;
+	globalChartRef.config.data.datasets[dataPoints.sun].data = [];
+	globalChartRef.config.data.datasets[dataPoints.moon].data = [];
+	if (document.getElementById("sunCheck").checked) {
+		initSunVector = sunVectorCalc(time);
+		if (dt === undefined) {
+			dt = 0;
+		}
+		let R = [[Math.cos(-long-theta0-n*dt), -Math.sin(-long-theta0-n*dt), 0],[Math.sin(-long-theta0-n*dt), Math.cos(-long-theta0-n*dt), 0],[0, 0, 1]];
+		initSunVector = math.multiply(R,initSunVector);
+		globalChartRef.config.data.datasets[dataPoints.sun].data.push({
+			x: 0,
+			y: 0
+		})
+		globalChartRef.config.data.datasets[dataPoints.sun].data.push({
+			x: initSunVector[1][0]*10,
+			y: initSunVector[0][0]*10
+		})
+	}
+	if (document.getElementById("moonCheck").checked) {
+		initSunVector = moonVectorCalc(time);
+		if (dt === undefined) {
+			dt = 0;
+		}
+		let R = [[Math.cos(-long-theta0-n*dt), -Math.sin(-long-theta0-n*dt), 0],[Math.sin(-long-theta0-n*dt), Math.cos(-long-theta0-n*dt), 0],[0, 0, 1]];
+		initSunVector = math.multiply(R,initSunVector);
+		globalChartRef.config.data.datasets[dataPoints.moon].data.push({
+			x: 0,
+			y: 0
+		})
+		globalChartRef.config.data.datasets[dataPoints.moon].data.push({
+			x: initSunVector[1][0]*10,
+			y: initSunVector[0][0]*10
+		})
+	}
+	globalChartRef.update();
+}
