072f0beb5a7bbe01b0818e58178c25d396b29ca1
pkevz90 <65626812+pkevz90@users.noreply.github.com>
2020-06-01 14:52:53 -0700

    added 3js

---
+++ app.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var mongo = require('mongodb');
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    if (q.pathname === '/') {q.pathname = '/index.html'}
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('C:/Users/pvanz/Documents/GitHub/pkevz.github.io' + q.pathname, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
