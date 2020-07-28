4086aa39c4cfced47d59b10b37ebf4cef1de9215
pkevz90 <65626812+pkevz90@users.noreply.github.com>
2020-07-28 13:48:11 -0700

    Daniels committing Capt VanZandt's work on leastSquares

--- js/leastSquares.js
+++ js/leastSquares.js
@@ -4,21 +4,73 @@ let app = new Vue({
     el: '#app-data',
     data: {
         points: [],
-        model: [],
+        modelOrder: 1,
         estimate: []
     },
     watch: {
         points: function() {
+			globalChartRef.config.data.datasets[1].data = [];
             for (point in this.points) {
                 globalChartRef.config.data.datasets[1].data.push({
                     x: this.points[point][0],
                     y: this.points[point][1]
                 });
-            }
+			}
+			if (this.points.length > 1) {
+				performLeastSquares();
+			}
+			
             globalChartRef.update();
         }
     }
 });
+
+function performLeastSquares() {
+	if (app.modelOrder === 1) {
+		let p = math.transpose(app.points);
+		let px = p[0];
+		let py = p[1];
+		let A = math.transpose(math.concat([math.ones(app.points.length)._data],[px],0));
+		let psuedoInvA = math.multiply(math.inv(math.multiply(math.transpose(A),A)),math.transpose(A))
+		let result = math.multiply(psuedoInvA,math.transpose(py))
+		globalChartRef.config.data.datasets[0].data = [];
+		globalChartRef.config.data.datasets[0].data.push({
+			x: 0,
+			y: result[1]*0+result[0]
+		},{
+			x: 40,
+			y: result[1]*40+result[0]
+		});
+	}
+	else if (app.modelOrder === 2) {
+		let p = math.transpose(app.points);
+		let px = p[0];
+		let py = p[1];
+		let A = math.transpose(math.concat([math.ones(app.points.length)._data],[px],[math.dotPow(px,2)],0));
+		let psuedoInvA = math.multiply(math.inv(math.multiply(math.transpose(A),A)),math.transpose(A))
+		let result = math.multiply(psuedoInvA,math.transpose(py))
+		console.log(result)
+		globalChartRef.config.data.datasets[0].data = [];
+		globalChartRef.config.data.datasets[0].data.push({
+			x: 0,
+			y: result[2]*0*0+result[1]*0+result[0]
+		},{
+			x: 10,
+			y: result[2]*10*10+result[1]*10+result[0]
+		},{
+			x: 20,
+			y: result[2]*20*20+result[1]*20+result[0]
+		},{
+			x: 30,
+			y: result[2]*30*30+result[1]*30+result[0]
+		},{
+			x: 40,
+			y: result[2]*40*40+result[1]*40+result[0]
+		});
+	}
+	
+	
+}
 
 function createGraph() {
 	var config = {
