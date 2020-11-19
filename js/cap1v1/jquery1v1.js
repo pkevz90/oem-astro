78cac90f15a966cc2f8d7ffc5cde31a31729c065
Your Name <you@example.com>
2020-11-18 20:40:07 -0800

    made teams all get updates at once

--- js/cap1v1/jquery1v1.js
+++ js/cap1v1/jquery1v1.js
@@ -249,7 +249,7 @@ $('.start-button').on('click', () => {
                         continue;
                     }
                     app.redTurn = inData[player].turn < app.redTurn ? inData[player].turn : app.redTurn;
-                    let turn = Math.min(Number($turn.text()), inData[player].turn);
+                    let turn = Math.min(inData['red'].turn,inData['blue'].turn,inData['green'].turn,inData['gray'].turn);
                     let oldNorm, newNorm, change = undefined, oldBurn = [...app.players[player].burns];
                     for (let ii = 0; ii < (turn - 1); ii++) {
                         oldNorm = math.norm(app.players[player].burns[ii]);
