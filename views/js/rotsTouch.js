fc12712ea3e10c8fcac80d62ff63bfdecfb5f388
Your Name <you@example.com>
2021-06-27 17:25:21 -0600

    adjusted touch zoom speed

--- views/js/rotsTouch.js
+++ views/js/rotsTouch.js
@@ -25,7 +25,7 @@ document.getElementById('canvas-div').addEventListener('touchmove', event => {
     event.preventDefault()
     console.log(event);
     windowOptions.origin_it_des += (event.changedTouches[0].clientX - windowOptions.touchHandler.start.x) * 2 / cnvs.width * windowOptions.width;
-    windowOptions.width_des *= (event.changedTouches[0].clientY - windowOptions.touchHandler.start.y) > 0 ? 1.01 : 0.9900990099009;
+    windowOptions.width_des *= (event.changedTouches[0].clientY - windowOptions.touchHandler.start.y) > 0 ? 1.02 : 0.98039215;
     windowOptions.touchHandler.start = {
         x: event.changedTouches[0].clientX,
         y: event.changedTouches[0].clientY
