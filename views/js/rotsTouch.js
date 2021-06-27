d49c3d081bb81d964dc4d9b072e6dcd83c534eb5
Your Name <you@example.com>
2021-06-27 12:50:08 -0600

    refined touch controls

--- views/js/rotsTouch.js
+++ views/js/rotsTouch.js
@@ -10,18 +10,20 @@ document.getElementById('canvas-div').addEventListener('touchstart', event => {
     if (windowOptions.touchHandler.n === 0) {
         setTimeout(() => {
             windowOptions.touchHandler.n = 0;
-        }, 500)
+        }, 250)
     }
     else if (windowOptions.touchHandler.n > 0) {
         keydownFunction({key: ' '})
         windowOptions.touchHandler.n = 0;
         setTimeout(() => {
             windowOptions.touchHandler.n = 0;
-        }, 500)
+        }, 250)
     }
     windowOptions.touchHandler.n++;
 })
 document.getElementById('canvas-div').addEventListener('touchmove', event => {
+    event.preventDefault()
+    console.log(event);
     windowOptions.origin_it_des += (event.changedTouches[0].clientX - windowOptions.touchHandler.start.x) * 2 / cnvs.width * windowOptions.width;
     windowOptions.width_des *= (event.changedTouches[0].clientY - windowOptions.touchHandler.start.y) > 0 ? 1.01 : 0.9900990099009;
     windowOptions.touchHandler.start = {
@@ -29,3 +31,7 @@ document.getElementById('canvas-div').addEventListener('touchmove', event => {
         y: event.changedTouches[0].clientY
     }
 })
+
+window.onwheel = () => {
+    console.log('hey');
+}
