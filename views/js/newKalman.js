8f064d46782308b263ac4426df14ba5490341e45
Your Name <you@example.com>
2021-08-26 07:26:23 -0600

    3d commit

--- views/js/newKalman.js
+++ views/js/newKalman.js
@@ -1,41 +1,41 @@
-
-let P = [[0.01, 0, 0, 0],[0, 0.01, 0, 0],[0, 0, 0.00000001, 0],[0, 0, 0, 0.00000001]];
-let angAmb = 0.01 * Math.PI / 180; // degrees
-let R = [[Math.pow(angAmb, 2)]];
-let Q = [[Math.pow(0.00001,2),0,0,0],[0,Math.pow(0.00001,2),0,0],[0,0,Math.pow(0.00000001,2),0],[0,0,0,Math.pow(0.00000001,2)]]
-let x = [[0], [10], [0.001], [0]];
-let xEst = [[2], [9], [0], [0]];
+let P = [10, 10, 10, 0.01, 0.01, 0.01];
+P = math.diag(P);
+let angAmb = 0.0001 * Math.PI / 180; // degrees
+let R = [[Math.pow(angAmb, 2), 0], [0, Math.pow(angAmb, 2)]];
+let Q = [Math.pow(0.0001,2), Math.pow(0.0001,2), Math.pow(0.0001,2), Math.pow(0.000001,2), Math.pow(0.000001,2), Math.pow(0.000001,2)]; 
+Q = math.diag(Q);
+let x = math.transpose([[0, 10, 0, 0.001, 0, 0.002]]);
+let xEst = math.transpose([[2, 9, 0, 0, 0, 0]]);
 let dt = 6;
-let onesP = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]];
+let onesP = math.identity(6)._data;
 let thrust = 0;
 
 
 window.addEventListener('keypress', () => {
     for (let ii = 0; ii < 50; ii++) {
-        let F = Fmatrix(dt);
-        // x = math.multiply(transitionMatrix(dt), x);
-        x = oneBurnFiniteHcw(x, {t: dt, a0: thrust});
-        xEst = oneBurnFiniteHcw(xEst, {t: dt, a0: thrust});
-        // xEst = math.multiply(transitionMatrix(dt), xEst);
+        let F = Fmatrix3(dt);
+        x = oneBurnFiniteHcw3(x, {t: dt, a0: thrust, phi: Math.PI / 2});
+        xEst = oneBurnFiniteHcw3(xEst, {t: dt, a0: thrust, phi: Math.PI / 2});
         P = math.add(math.multiply(F,P,math.transpose(F)),Q);
-        let zAct = measEst(x);
-        let zEst = measEst(xEst) + normalRandom() * angAmb;
-        let H = Hmatrix(xEst);
-        if (zAct*zEst < 0 && Math.abs(zEst) > 1) {
-            if (zEst < 0) {
-                zEst += 2*Math.PI;
+        let zAct = measEst3(x);
+        let zEst = measEst3(xEst);
+        zEst[1][0] += normalRandom() * angAmb;
+        zEst[0][0] += normalRandom() * angAmb;
+        let H = Hmatrix3(xEst);
+        if (zAct[0][0]*zEst[0][0] < 0 && Math.abs(zEst[0][0]) > 1) {
+            if (zEst[0][0] < 0) {
+                zEst[0][0] += 2*Math.PI;
             }
             else {
-                zAct += 2*Math.PI;
+                zAct[0][0] += 2*Math.PI;
             }
         }
-        let K  = Kmatrix(P,H,R[0][0]);
-        K = [[K[0]],[K[1]],[K[2]],[K[3]]];
-        P = math.multiply(math.subtract(onesP,math.multiply(K,[H])),P);
+        let K  = Kmatrix(P,H,R);
+        P = math.multiply(math.subtract(onesP,math.multiply(K,H)),P);
         let y = math.subtract(zAct, zEst);
         xEst = math.add(xEst, math.multiply(K, y));
     }
-    console.log(math.squeeze(x), math.squeeze(xEst));
+    console.log(math.norm(math.subtract(math.squeeze(x), math.squeeze(xEst))));
 
 })
 
@@ -44,26 +44,25 @@ function Kmatrix(P,H,R) {
     return math.multiply(math.multiply(P,math.transpose(H)),math.inv(S));
 }
 
-function Hmatrix(x) {
-    let x12 = math.pow(x[0][0], 2) + math.pow(x[1][0],2);
-    return [-x[1][0]/x12, x[0][0]/x12, 0, 0];
+function Hmatrix3(x) {
+    x = math.squeeze(x);
+    let x12 = math.pow(x[0], 2) + math.pow(x[1],2);
+    return [[-x[1]/x12, x[0]/x12, 0, 0, 0, 0],[-x[2]*x[0] / Math.sqrt(x12) / (x12 + x[2]*x[2]), -x[2]*x[1] / Math.sqrt(x12) / (x12 + x[2]*x[2]), Math.sqrt(x12) / (x12 + x[2]*x[2]), 0, 0, 0]];
 }
 
-function Fmatrix(t, n = 2 * Math.PI / 86164) {
-    return [[1,0,t,0],
-            [0,1,0,t],
-            [3*n*n*t,0,1,2*n*t],
-            [0,0,-2*n*t,1]];
+function Fmatrix3(t, n = 2 * Math.PI / 86164) {
+    return [[1,0,0,t,0,0],
+            [0,1,0,0,t,0],
+            [0,0,1,0,0,t],
+            [3*n*n*t,0,0,1,2*n*t, 0],
+            [0,0,0,-2*n*t,1, 0],
+            [0,0,-n*n*t,0,0,1]];
 }
 
-function measEst(x) {
-    return Math.atan2(x[1][0],x[0][0]);
+function measEst3(x) {
+    return [[Math.atan2(x[1][0],x[0][0])], [Math.atan2(x[2][0], Math.sqrt(x[1][0]*x[1][0]+x[0][0]*x[0][0]))]];
 }
 
-function transitionMatrix(dt,n = 2*Math.PI/86164) {
-    let nt = n*dt; let cnt = Math.cos(nt); let snt = Math.sin(nt);
-    return [[4-3*cnt, 0, snt/n, 2*(1-cnt)/n],[6*(snt-nt), 1, 2*(cnt-1)/n, (4*snt-3*nt)/n],[3*n*snt, 0, cnt, 2*snt],[6*n*(cnt-1), 0, -2*snt, 4*cnt-3]];
-}
 
 function normalRandom()
 {
@@ -93,22 +92,28 @@ function normalRandom()
 	return val;
 }
 
-function oneBurnFiniteHcw(state, options) {
-    let {alpha = 10, tB = 1, t = 3600, n = 2*Math.PI / 86164, a0 = 0.00001} = options;
+function oneBurnFiniteHcw3(state, options) {
+    let {alpha = 10, phi = 0, tB = 1, t = 3600, n = 2*Math.PI / 86164, a0 = 0.00001} = options;
     x0 = state[0][0];
-    xd0 = state[2][0];
+    xd0 = state[3][0];
     y0 = state[1][0];
-    yd0 = state[3][0];
+    yd0 = state[4][0];
+    z0 = state[2][0];
+    zd0 = state[5][0];
     // console.log(x0,y0,z0)
-    let xM = radialPosClosed(x0, xd0, yd0, a0, alpha, 0, tB * t, n);
-    let xdM = radialVelClosed(x0, xd0, yd0, a0, alpha, 0, tB * t, n);
-    let yM = intrackPosClosed(x0, xd0, y0, yd0, a0, alpha, 0, tB * t, n);
-    let ydM = intrackVelClosed(x0, xd0, yd0, a0, alpha, 0, tB * t, n);
+    let xM = radialPosClosed(x0, xd0, yd0, a0, alpha, phi, tB * t, n);
+    let xdM = radialVelClosed(x0, xd0, yd0, a0, alpha, phi, tB * t, n);
+    let yM = intrackPosClosed(x0, xd0, y0, yd0, a0, alpha, phi, tB * t, n);
+    let ydM = intrackVelClosed(x0, xd0, yd0, a0, alpha, phi, tB * t, n);
+    let zM = crosstrackPosClosed(z0, zd0, a0, phi, tB * t, n);
+    let zdM = crosstrackVelClosed(z0, zd0, a0, phi, tB * t, n);
     let xF = radialPosClosed(xM, xdM, ydM, 0, 0, 0, t - tB * t, n);
     let xdF = radialVelClosed(xM, xdM, ydM, 0, 0, 0, t - tB * t, n);
     let yF = intrackPosClosed(xM, xdM, yM, ydM, 0, 0, 0, t - tB * t, n);
     let ydF = intrackVelClosed(xM, xdM, ydM, 0, 0, 0, t - tB * t, n);
-    return [[xF], [yF], [xdF], [ydF]]
+    let zF = crosstrackPosClosed(zM, zdM, 0, 0, t - tB * t, n);
+    let zdF = crosstrackVelClosed(zM, zdM, 0, 0, t - tB * t, n);
+    return [[xF], [yF], [zF], [xdF], [ydF], [zdF]]
 
 }
 
@@ -159,5 +164,6 @@ function crosstrackVelClosed(z0, zd0, a0, phi, t, n) {
 }
 
 function resetP() {
-    P = [[0.01, 0, 0, 0],[0, 0.01, 0, 0],[0, 0, 0.00000001, 0],[0, 0, 0, 0.00000001]];
+    P = [1, 1, 1, 0.01, 0.01, 0.01];
+    P = math.diag(P);   
 }
