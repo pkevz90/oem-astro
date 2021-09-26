895b4ba31be85465dfedb290c8a63686070e46c8
Your Name <you@example.com>
2021-09-26 13:01:04 -0600

    a

--- views/js/pathfinding.js
+++ views/js/pathfinding.js
@@ -12,9 +12,9 @@ class Grid {
     explored = [];
     smallest = [];
     constructor(options = {}) {
-        let {spaces = 50, start = [1,1], target = [Math.floor(Math.random() * 50), Math.floor(Math.random() * 50)]} = options;
+        let {spaces = 50, start = [1,1]} = options;
         this.start = start;
-        this.target = target;
+        this.target =  [Math.floor(Math.random() * spaces), Math.floor(Math.random() * spaces)];
         this.produceGrid(spaces);
         this.grid[start[0]][start[1]].g = 0;
         this.grid[start[0]][start[1]].h = 0;
@@ -43,17 +43,12 @@ class Grid {
             for (let jj = 0; jj < this.grid[ii].length; jj++) {
                 if (!this.grid[jj][ii].passable) ctx.fillStyle = 'black';
                 else if (this.grid[jj][ii].road) ctx.fillStyle = 'brown';
-                else if ((jj === this.start[0] && ii === this.start[1])) ctx.fillStyle = 'orange';
-                else if ((jj === this.start[0] && ii === this.start[1]) || this.checkExplored(jj,ii)) ctx.fillStyle = 'green';
-                else if ((jj === this.target[0] && ii === this.target[1])) ctx.fillStyle = 'orange';
-                else if ((jj === this.target[0] && ii === this.target[1]) || this.grid[jj][ii].computed) ctx.fillStyle = 'red';
+                else if ((jj === this.start[0] && ii === this.start[1])) ctx.fillStyle = 'rgb(255,168,0)';
+                else if ((jj === this.target[0] && ii === this.target[1])) ctx.fillStyle = 'rgb(255,168,0)';
                 else ctx.fillStyle = 'white';
                 ctx.strokeRect(jj * width / this.grid[ii].length, ii * height / this.grid.length, (jj+1) * width / this.grid[ii].length, (ii+1) * height / this.grid.length)
                 ctx.fillRect(jj * width / this.grid[ii].length, ii * height / this.grid.length, (jj+1) * width / this.grid[ii].length, (ii+1) * height / this.grid.length)
             
-                if (this.grid[jj][ii].computed) {
-                    ctx.strokeText(this.grid[jj][ii].getTotal().toFixed(1), (jj+0.5) * width / this.grid[jj].length, (ii+0.5) * height / this.grid.length)
-                }
             }
         }
     }
@@ -131,9 +126,14 @@ class Grid {
         for (let ii = 0; ii < this.grid.length; ii++) {
             for (let jj = 0; jj < this.grid[ii].length; jj++) {
                 if ((ii === this.start[1] && jj === this.start[0]) || (ii === this.target[1] && jj === this.target[0])) continue;
-                this.grid[jj][ii].passable = Math.random() < 0.75 ? true : false;
+                let isWallNear = !this.grid[jj-1]?.[ii]?.passable;
+                
+                let isRoadNear = this.grid[jj]?.[ii-1]?.road;
+                let notNext = this.grid[jj-1]?.[ii]?.road
+                console.log(isWallNear);
+                this.grid[jj][ii].passable = Math.random() < (isWallNear ? 0.4 : 0.95) ? true : false;
                 if (this.grid[jj][ii].passable) {
-                    // this.grid[jj][ii].road = Math.random() < 0.25 ? true : false;
+                    this.grid[jj][ii].road = Math.random() < (notNext ? 0: 1)*(isRoadNear ? 0.75 : 0.05) ? true : false;
                 }
             }
         }
@@ -173,7 +173,7 @@ class Space {
     computeCost(target, parent) {
         this.computed = true;
         let roadM = mainGrid.grid[parent[0]][parent[1]].road && this.road ? 0.75 : 1;
-        let g = roadM*mainGrid.grid[parent[0]][parent[1]].g + ((this.coor[0] - parent[0]) ** 2 + (this.coor[1] - parent[1]) ** 2) ** (1/2);
+        let g = mainGrid.grid[parent[0]][parent[1]].g + roadM*((this.coor[0] - parent[0]) ** 2 + (this.coor[1] - parent[1]) ** 2) ** (1/2);
         let h = h_constant*((this.coor[0] - target[0]) ** 2 + (this.coor[1] - target[1]) ** 2) ** (1/2);
         // let h = 0;
         if ((g + h) < this.getTotal() && !mainGrid.checkExplored(this.coor[0], this.coor[1])) {
@@ -181,19 +181,23 @@ class Space {
             this.h = h;
             this.parent = parent;
         }
+        
+        ctx.font = 'bold ' + cnvs.height * 0.5 / mainGrid.grid.length + 'px serif';
         ctx.fillStyle = mainGrid.checkExplored(this.coor[0], this.coor[1]) ? 'green' : 'red';
+        ctx.fillStyle = (this.coor[0] === mainGrid.start[0] && this.coor[1] === mainGrid.start[1]) || (this.coor[0] === mainGrid.target[0] && this.coor[1] === mainGrid.target[1]) ? 'orange' : ctx.fillStyle;
         // console.log(this.coor);
         ctx.strokeRect(this.coor[0] * cnvs.width / mainGrid.grid[this.coor[1]].length, this.coor[1] * cnvs.height / mainGrid.grid.length,cnvs.width / mainGrid.grid[this.coor[1]].length,cnvs.height / mainGrid.grid.length)
         ctx.fillRect(this.coor[0] * cnvs.width / mainGrid.grid[this.coor[1]].length, this.coor[1] * cnvs.height / mainGrid.grid.length, cnvs.width / mainGrid.grid[this.coor[1]].length, cnvs.height / mainGrid.grid.length)
      
         ctx.fillStyle = 'black';
-        ctx.fillText(this.getTotal().toFixed(1), (this.coor[0]+0.5) * cnvs.width / mainGrid.grid[this.coor[0]].length, (this.coor[1]+0.5) * cnvs.height / mainGrid.grid.length)
-        ctx.lineWidth = 2;
-        ctx.beginPath()
-        ctx.moveTo((0.5+this.coor[0]) * cnvs.width / mainGrid.grid[this.coor[1]].length, (0.5+this.coor[1]) * cnvs.height / mainGrid.grid.length)
-        ctx.lineTo((0.5+parent[0]) * cnvs.width / mainGrid.grid[parent[1]].length, (0.5+parent[1]) * cnvs.height / mainGrid.grid.length)
-        ctx.stroke();
-        ctx.lineWidth = 1;
+        let text = this.coor[0] === mainGrid.start[0] && this.coor[1] === mainGrid.start[1] ? 'S' : this.coor[0] === mainGrid.target[0] && this.coor[1] === mainGrid.target[1] ? 'T' :  this.getTotal().toFixed(1);
+        ctx.fillText(text, (this.coor[0]+0.5) * cnvs.width / mainGrid.grid[this.coor[0]].length, (this.coor[1]+0.5) * cnvs.height / mainGrid.grid.length)
+        // ctx.lineWidth = 2;
+        // ctx.beginPath()
+        // ctx.moveTo((0.5+this.coor[0]) * cnvs.width / mainGrid.grid[this.coor[1]].length, (0.5+this.coor[1]) * cnvs.height / mainGrid.grid.length)
+        // ctx.lineTo((0.5+parent[0]) * cnvs.width / mainGrid.grid[parent[1]].length, (0.5+parent[1]) * cnvs.height / mainGrid.grid.length)
+        // ctx.stroke();
+        // ctx.lineWidth = 1;
     }
 }
 
@@ -211,18 +215,19 @@ cnvs.addEventListener('click', el => {
             
             if (loc[0] === mainGrid.target[0] && loc[1] === mainGrid.target[1]) {
                 loc = [mainGrid.target[0],mainGrid.target[1]];
+                loc = mainGrid.grid[loc[0]][loc[1]].parent;
                 let ii = 0;
                 ctx.fillStyle = 'orange';
                 let width = cnvs.width;
                 let height = cnvs.height;
                 while (loc[0] !== mainGrid.start[0] || loc[1] !== mainGrid.start[1]) {
-                    loc = mainGrid.grid[loc[0]][loc[1]].parent;
-                    // console.log(loc);
                     ctx.strokeRect(loc[0] * width / mainGrid.grid[loc[1]].length, loc[1] * height / mainGrid.grid.length, width / mainGrid.grid[loc[1]].length, height / mainGrid.grid.length)
                     ctx.fillRect(loc[0] * width / mainGrid.grid[loc[1]].length, loc[1] * height / mainGrid.grid.length, width / mainGrid.grid[loc[1]].length, height / mainGrid.grid.length)
+                    loc = mainGrid.grid[loc[0]][loc[1]].parent;
                     if (ii >= 10000) break;
                     ii++;
                 }
+                console.log(ii);
                 clearInterval(a);
                 return;
             }
