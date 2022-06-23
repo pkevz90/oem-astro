e3d95fc7db61deec4f5dddc110b43492d52822c8
PVZ <pvanza12@gmail.com>
2022-06-22 20:55:42 -0600

    initial commit for next gen SDA system, tle randomizer updates

---
+++ views/js/sda_v2.js
@@ -0,0 +1,8 @@
+let mainWindow = {
+    planet: {
+        mu: 398600.4418,
+        r: 6371,
+        w: 2 * Math.PI / 86164
+    },
+    
+}
--- views/js/tleRandomizer.js
+++ views/js/tleRandomizer.js
@@ -18,8 +18,7 @@ document.getElementsByTagName('td')[0].getElementsByTagName('input')[0].addEvent
 })
 
 
-function normalRandom()
-{
+function normalRandom() {
     var val, u, v, s, mul;
     spareRandom = null;
     if(spareRandom !== null)
@@ -46,53 +45,26 @@ function normalRandom()
     return val;
 }
 
-function randn() {
-    var u = 0, v = 0;
-    while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
-    while(v === 0) v = Math.random();
-    return Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
-}
-
-function testRandn(n = 1000) {
-    let as = []
-    for (let ii = 0; ii < n; ii++) {
-        as.push(randn() * 2 + 1)
-    }
-
-    let mean = as.reduce((a,b) => a + b) / as.length
-
-    let std = (as.map(a => (a - mean) ** 2).reduce((a,b) => a + b) / as.length) ** (1/2)
-    console.log(mean,std);
-}
-
 function exportFile() {
     let files = window.localStorage.files === undefined ? {} : JSON.parse(window.localStorage.files)
     let err_inputs = document.getElementsByTagName('input');
     let desDate = new Date(Number(err_inputs[2].value), Number(err_inputs[0].value) - 1, Number(err_inputs[1].value), Number(err_inputs[3].value), Number(err_inputs[4].value));
-    if (desDate == 'Invalid Date') return;
+    if (desDate == 'Invalid Date') return alert('Date not in proper format');
     let timeDiff = desDate - epochDate;
     timeDiff /= 1000;
-    console.log(timeDiff)
     if (timeDiff < 0) return alert('Time must be during the imported .e file')
-    let timeLine = data[1].findIndex(line => {
-        return Number(line.split(/ +/)[1]) >= timeDiff;
-    });
-    // Get propTime
+    let timeLine = data[1].findIndex(line => Number(line.split(/ +/).filter(s => s !== '')[0]) >= timeDiff)
+    // Get prop end time from last line in file, found by finding last line in file with valid time
     let lastLine = data[1].length-1
-    while (isNaN(Number(data[1][lastLine].split(/ +/).filter(s => s !== '')[0]))) {
-        lastLine--
-    }
+    while (isNaN(Number(data[1][lastLine].split(/ +/).filter(s => s !== '')[0]))) lastLine--
     let endTime = Number(data[1][lastLine].split(/ +/).filter(s => s !== '')[0])
-
     let saveName = 'sat' + math.floor(data[1][0].split(/ +/).filter(line => line !== '')[1]);
     // Pull initial state from ephemeris file
     let stateEphemeris = data[1].slice(timeLine + 1, timeLine + 2)
     state = stateEphemeris[0].split(/ +/).filter(s => s !== '').map(s => Number(s))
     let t = state.shift()
     //Prop initial state state back to desired time (to avoid any potential burn at desired epoch)
-    for (let ii = 0; ii < 10; ii++) {
-        state = runge_kutta(-(t - timeDiff) / 10, [state])
-    }
+    for (let ii = 0; ii < 10; ii++) state = runge_kutta(-(t - timeDiff) / 10, [state])
     t = timeDiff
     // Add gaussian error to the initial state
     let rEci2Eci = Ric2EciRedux(state.slice(0,3), state.slice(3,6))
@@ -107,28 +79,29 @@ function exportFile() {
     state = math.add(state, initError)
     // Define initial covariance matrix
     let pEphemeris = []
-    pEphemeris.push(`${t} ${P[0][0].toExponential(16)} ${P[0][1].toExponential(16)} ${P[0][2].toExponential(16)} ${P[1][1].toExponential(16)} ${P[1][2].toExponential(16)} ${P[2][2].toExponential(16)}`)
-    stateEphemeris = [`${t.toExponential(16)} ${state[0].toExponential(16)} ${state[1].toExponential(16)} ${state[2].toExponential(16)} ${state[3].toExponential(16)} ${state[4].toExponential(16)} ${state[5].toExponential(16)}`]
-
+    pEphemeris.push(`${t.toExponential(16)} ${P[0][0].toExponential(16)} ${P[0][1].toExponential(16)} ${P[0][2].toExponential(16)} ${P[1][1].toExponential(16)} ${P[1][2].toExponential(16)} ${P[2][2].toExponential(16)}`)
+    stateEphemeris = [toExponentialDigits(t, 16, 2) + ' ' + state.map(s => toExponentialDigits(s, 16, 2)).join(' ')]
     let timeDelta = 120
     let propTime = (endTime - timeDiff) > 86400 ? 86400 : endTime - timeDiff
     for (let ii = timeDelta; ii <= propTime; ii+=timeDelta) {
         let out = propCovariance(P, timeDelta, [state])
         state = out.state
         P = out.P
-        stateEphemeris.push(`${(t + ii).toExponential(16)} ${state[0].toExponential(16)} ${state[1].toExponential(16)} ${state[2].toExponential(16)} ${state[3].toExponential(16)} ${state[4].toExponential(16)} ${state[5].toExponential(16)}`); 
+        stateEphemeris.push(toExponentialDigits(t + ii, 16, 2) + ' ' + state.map(s => toExponentialDigits(s, 16, 2)).join(' '))
         pEphemeris.push(`${(t + ii).toExponential(16)} ${P[0][0].toExponential(16)} ${P[0][1].toExponential(16)} ${P[0][2].toExponential(16)} ${P[1][1].toExponential(16)} ${P[1][2].toExponential(16)} ${P[2][2].toExponential(16)}`)
         if (isNaN(state[0])) {
-
-            console.error('Error in state propagation');
-            // console.log(P, state);
+            alert('Error in state propagation');
             return
         }
     }
     // Remove number of ephemeris points to default to read all points
-    // let header = data[0].split(/\n/).filter(line => line.search('NumberOfEp') === -1).join('\n')
     let header = data[0]
     if (files[saveName] === undefined) {
+        // If file is not in records, fill in past with truth
+        while (timeLine > 0) {
+            timeLine--
+            stateEphemeris.unshift(data[1][timeLine].split(/ +/).filter(s => s !== '').join(' '))
+        }
         files[saveName] = {
             header,
             data: [{posVelData: stateEphemeris, covData: pEphemeris, startTime: timeDiff, time: new Date() - 0}]
@@ -149,7 +122,6 @@ function exportConsolidated(satelliteData, name) {
     files = files.sort((a, b) => a.startTime - b.startTime)
     for (let file = 0; file < files.length; file++) {
         let endTime = file === files.length - 1 ? 1e10 : files[file + 1].startTime
-        // console.log(endTime);
         files[file].posVelData = files[file].posVelData.filter(data => Number(data.split(/ +/)[0]) < endTime)
         files[file].covData = files[file].covData.filter(data => Number(data.split(/ +/)[0]) < endTime)
         files[file].posVelData = files[file].posVelData.join('\n')
@@ -327,47 +299,6 @@ function padNumbers(n) {
     return n
 }
 
-function modelCov(n_ground = 2, n_space = 1, time = 5400) {
-    let p = []
-    // p[n_ground][n_space]
-    p = null
-    p = null
-    p = [
-        [13975.909, 982.571, 744.346, 12.361, 1.935, 0.71],
-        [8366.232, 790.046, 541.382, 3.128, 0.906, 0.258],
-        [6390.515, 691.149, 458.905, 1.308, 0.552, 0.145],
-        [5425.6994, 620.201, 405.610, 0.711, 0.38, 0.096],
-        [5061.81, 590.399, 380.624, 0.501, 0.297, 0.071],
-        [4221.767, 498.078, 314.579, 0.371, 0.219, 0.048],
-        [3955.416, 477.512, 292.136, 0.341, 0.196, 0.038],
-        [3812.183, 483.021, 285.744, 0.336, 0.192, 0.032],
-        [3355.082, 460.022, 255.481, 0.303, 0.18, 0.025]
-    ]
-    p = [
-        [836.999, 687.854, 512.440, 0.819, 0.638, 0.489],
-        [655.257, 527.588, 396.966, 0.33, 0.242, 0.189],
-        [567.927, 447.685, 339.985, 0.197, 0.137, 0.108],
-        [506.116, 390.520, 298.985, 0.135, 0.092, 0.071],
-        [441.051, 333.24, 256.846, 0.097, 0.066, 0.048],
-        [420.870, 311.652, 241.401, 0.08, 0.054, 0.037],
-        [385.975, 280.486, 217.887, 0.064, 0.046, 0.028],
-        [351.072, 250.825, 194.932, 0.053, 0.039, 0.022],
-        [342.24, 240.936, 186.811, 0.047, 0.037, 0.018]
-    ]
-    p = [
-        [260.726, 738.205, 237.811, 0.274, 0.696, 0.227],
-        [182.585, 493.521, 161.177, 0.111, 0.228, 0.077],
-        [157.512, 401.427, 133.793, 0.073, 0.119, 0.042],
-        [143.519, 342.614, 117.096, 0.055, 0.073, 0.028],
-        [130.011, 290.085, 101.995, 0.043, 0.046, 0.019],
-        [127.782, 266.778, 96.635, 0.038, 0.033, 0.015],
-        [121.136, 237.417, 88.592, 0.032, 0.024, 0.012],
-        [116.479, 215.317, 82.636, 0.028, 0.018, 0.009],
-        [112.967, 198.032, 77.966, 0.025, 0.014, 0.008]
-    ]
-    console.log(p);
-}
-
 function induceBurnError(event) {
     let inputs = event.target.parentElement.parentElement.getElementsByTagName('input');
     let r = Number(inputs[0].value)
@@ -479,4 +410,14 @@ function Ric2EciRedux(rC = [(398600.4418 / n ** 2)**(1/3), 0, 0], drC = [0, (398
     let R = math.concat(R1, R2, 0)
     console.log(R);
     return R
+}
+
+function toExponentialDigits(inNumber = 1234, nDig = 3, nExp = 2) {
+    inNumber = inNumber.toExponential(nDig)
+    inNumber = inNumber.split('e')
+    inNumber = [inNumber[0], inNumber[1].slice(0,1), inNumber[1].slice(1)]
+    while (inNumber[2].length < nExp) {
+        inNumber[2] = 0 + inNumber[2]
+    }
+    return inNumber[0] + 'e' + inNumber[1] + inNumber[2]
 }
