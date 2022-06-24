a7744c17326af3ca829ada6bfe349d20697cbe0d
PVZ <pvanza12@gmail.com>
2022-06-23 22:44:36 -0600

    checkpoint for sda v2

--- views/js/sda_v2.js
+++ views/js/sda_v2.js
@@ -5,11 +5,12 @@ let mainWindow = {
         w: 2 * Math.PI / 86164,
         sidAngle: 0 //at t = 0
     },
-    maxTime: 4 * 3600, //seconds
+    initSun: [-94309.749762,    139482969.590637,    60466737.314102],
+    maxTime: 8 * 3600, //seconds
+    propTime: 60,
     satellites: [
         {
             origState: [42164, 0, 0, 0, 3.0629662506494473, 0.2679748236943305],
-            stateHist: [],
             obFreq: 900,
             obs: []
         }
@@ -18,23 +19,44 @@ let mainWindow = {
         {
             type: 'optical',
             noise: {
-                angle: 0.005 //deg
+                angle: 0.05 //deg
             },
-            lat: 40, //deg
+            lat: -20, //deg
             long: 30, //deg
             azMask: [0, 360],
-            elMask: [0,90]
+            elMask: [5,90]
         },
         {
             type: 'optical',
             noise: {
-                angle: 0.0005 //deg
+                angle: 0.005, //deg
             },
             lat: 40, //deg
-            long: -30, //deg
+            long: -60, //deg
             azMask: [0, 360],
-            elMask: [0,90]
+            elMask: [5,90]
+        },
+        {
+            type: 'optical',
+            noise: {
+                angle: 0.005, //deg
+            },
+            lat: 20, //deg
+            long: -130, //deg
+            azMask: [0, 360],
+            elMask: [5,90]
         }
+        // {
+        //     type: 'radar',
+        //     noise: {
+        //         angle: 0.5, //deg
+        //         r: 5 //km
+        //     },
+        //     lat: 40, //deg
+        //     long: -30, //deg
+        //     azMask: [0, 360],
+        //     elMask: [0,90]
+        // }
     ]
 }
 
@@ -60,43 +82,6 @@ function orbitalDynamics(position = [42164, 0, 0, 0, 3.074, 0], j2Eff = false) {
         -mu * y / r ** 3 ,
         -mu * z / r ** 3 
     ]
-}
-
-function calcAzEl(sat = 0, time = 0, options ={}) {
-    let {noise = false, mask = false} = options
-    let steps = math.ceil(math.abs(time) / 250)
-    let propSatState
-    if (sat.length === undefined) {
-        propSatState = mainWindow.satellites[sat].origState.slice()
-    }
-    else {
-        propSatState = sat.slice()
-    }
-    for (let index = 0; index < steps; index++) {
-        propSatState =  runge_kutta(propSatState, time / steps)
-    }
-    let curSidTime = mainWindow.planet.sidAngle + time * mainWindow.planet.w
-    let sidRot = rotationMatrices(curSidTime * 180 / Math.PI, 3)
-    let obs = []
-    for (let index = 0; index < mainWindow.sensors.length; index++) {
-        let sensorPos = math.dotMultiply(mainWindow.planet.r, [math.cos(mainWindow.sensors[index].long * Math.PI / 180) * math.cos(mainWindow.sensors[index].lat * Math.PI / 180),
-                         math.sin(mainWindow.sensors[index].long * Math.PI / 180) * math.cos(mainWindow.sensors[index].lat * Math.PI / 180),
-                         math.sin(mainWindow.sensors[index].lat * Math.PI / 180)])
-        sensorPos = math.squeeze(math.multiply(sidRot, sensorPos))
-        let topoZ = math.dotDivide(sensorPos, math.norm(sensorPos))
-        let topoX = math.cross([0,0,1], topoZ)
-        topoX = math.dotDivide(topoX, math.norm(topoX))
-        topoY = math.cross(topoZ, topoX)
-        let rEciToTopo = rotMatrixFrames([topoX, topoY, topoZ])
-        let relativeSatState = math.subtract(propSatState.slice(0,3), sensorPos)
-        relativeSatState = math.squeeze(math.multiply(rEciToTopo, math.transpose([relativeSatState])))
-        let satAz = math.atan2(relativeSatState[0], relativeSatState[1])
-        let satEl = math.atan2(relativeSatState[2], math.norm(relativeSatState.slice(0,2)))
-        let satRange = math.norm(relativeSatState)
-        if (mainWindow.sensors[index].type === 'optical') obs.push({sensor: index, time, data: [satAz, satEl], noise: [mainWindow.sensors[index].noise.angle, mainWindow.sensors[index].noise.angle].map(s => s * Math.PI / 180)})
-        if (mainWindow.sensors[index].type === 'radar') obs.push({sensor: index, time, data: [satAz, satEl, satRange], noise: [mainWindow.sensors[index].noise.angle, mainWindow.sensors[index].noise.angle, , mainWindow.sensors[index].noise.range]})
-    }
-    return obs
 }
 
 function rotMatrixFrames(frameTo = frameFrom = math.identity(3)._data, frameFrom = math.identity(3)._data) {
@@ -139,28 +124,102 @@ function estimateCovariance(sat = 0) {
     let blsMatrices = createJacobian(sat)
     let a = math.inv(math.multiply(math.transpose(blsMatrices.jac), blsMatrices.w, blsMatrices.jac))
     console.log(choleskyDecomposition(a));
+    return a
+}
+
+function checkSensors(sat = [], time, options ={}) {
+    let {noise = false, mask = true, pastObs = [], obLimit = 900, sensors = math.range(0, mainWindow.sensors.length)._data} = options
+    let propSatState = sat.slice()
+    let curSidTime = mainWindow.planet.sidAngle + time * mainWindow.planet.w
+    let sidRot = rotationMatrices(curSidTime * 180 / Math.PI, 3)
+    let sunPos = math.subtract(math.squeeze(math.multiply(rotationMatrices(360 * time / 31556952 , 3), math.transpose([mainWindow.initSun]))), sat.slice(0,3))
+    let sunPosUnit = math.dotDivide(sunPos, math.norm(sunPos))
+    let obs = []
+    for (let ii = 0; ii < sensors.length; ii++) {
+        let index = sensors[ii]
+        if (mainWindow.sensors[index].type === 'optical' || mainWindow.sensors[index].type === 'radar') {
+            let pastSensorObs = pastObs.filter(s => s.sensor === index).filter(ob => math.abs(ob.time - time) < obLimit)
+            if (pastSensorObs.length > 0 && mask) continue
+            let sensorPos = math.dotMultiply(mainWindow.planet.r, [math.cos(mainWindow.sensors[index].long * Math.PI / 180) * math.cos(mainWindow.sensors[index].lat * Math.PI / 180),
+                             math.sin(mainWindow.sensors[index].long * Math.PI / 180) * math.cos(mainWindow.sensors[index].lat * Math.PI / 180),
+                             math.sin(mainWindow.sensors[index].lat * Math.PI / 180)])
+            sensorPos = math.squeeze(math.multiply(sidRot, sensorPos))
+            let topoZ = math.dotDivide(sensorPos, math.norm(sensorPos))
+            let topoX = math.cross([0,0,1], topoZ)
+            topoX = math.dotDivide(topoX, math.norm(topoX))
+            let topoY = math.cross(topoZ, topoX)
+            let rEciToTopo = rotMatrixFrames([topoX, topoY, topoZ])
+            let relativeSatState = math.subtract(propSatState.slice(0,3), sensorPos)
+            // Check if sun behind optical sensor
+            let cats = math.acos(math.dot(relativeSatState, sunPosUnit) / math.norm(relativeSatState)) * 180 / Math.PI
+            if (cats < 110 && mainWindow.sensors[index].type === 'optical' && mask) continue
+            // Check if satellite in direct sunlight
+            // let satCats = math.acos(math.dot(math.dotMultiply(-1, sat.slice(0,3)), sunPosUnit) / math.norm(sat.slice(0,3)))
+            // satCats = math.norm(sat.slice(0,3)) * math.tan(satCats)
+            // console.log(satCats, time);
+            // if (satCats < 6700 && mask) continue
+            let check = lineSphereIntercetionBool(sunPosUnit, sat.slice(0,3), [0,0,0], sphereRadius=6500)
+            if (check & mask) continue
+            relativeSatState = math.squeeze(math.multiply(rEciToTopo, math.transpose([relativeSatState])))
+            let satAz = math.atan2(relativeSatState[0], relativeSatState[1])
+            let satEl = math.atan2(relativeSatState[2], math.norm(relativeSatState.slice(0,2)))
+            // Check if above horizon
+            if ((satEl * 180 / Math.PI) < mainWindow.sensors[index].elMask[0]) continue
+            let satRange = math.norm(relativeSatState)
+            if (mainWindow.sensors[index].type === 'optical' ) {
+                obs.push({
+                    sensor: index,
+                    time,
+                    obs: [satAz, satEl],
+                    noise: [mainWindow.sensors[index].noise.angle, mainWindow.sensors[index].noise.angle].map(n => n * Math.PI / 180)
+                })
+            }
+            else if (mainWindow.sensors[index].type === 'radar' ) {
+                obs.push({
+                    sensor: index,
+                    time,
+                    obs: [satAz, satEl, satRange],
+                    noise: [mainWindow.sensors[index].noise.angle * Math.PI / 180, mainWindow.sensors[index].noise.angle * Math.PI / 180, mainWindow.sensors[index].noise.r]
+                })
+            }
+        }
+        else if (mainWindow.sensors[index].type === 'space') {
+
+        }
+    }
+    return obs
 }
 
 function getObHistory(sat = 0) {
     let timeOb = 0
     mainWindow.satellites[sat].obs = []
+    let propState = mainWindow.satellites[sat].origState.slice()
+    let satObs = []
     while (timeOb < mainWindow.maxTime) {
-        let ob = calcAzEl(0, -timeOb)
-        mainWindow.satellites[sat].obs.push(...ob)
-        timeOb += mainWindow.satellites[sat].obFreq
+        let obs = checkSensors(propState, -timeOb, {pastObs: satObs, obLimit: mainWindow.satellites[sat].obFreq})
+        propState =  runge_kutta(propState, -mainWindow.propTime)
+        satObs.push(...obs)
+        timeOb += mainWindow.propTime
     }
+    mainWindow.satellites[sat].obs = satObs
 }
 
 function copyObHistory(sat, state) {
     if (state === undefined) {
         state = mainWindow.satellites[sat].origState
-        state[0] += 10
     }
+    let propState = state.slice()
     let times = mainWindow.satellites[sat].obs.map(o => o.time)
+    let propTime = 0
     let outObs = []
-    for (let ii = 0; ii < times.length; ii++) {
-        let ob = calcAzEl(state, times[ii])
-        outObs.push(...ob)
+    times.unshift(0)
+    for (let ii = 0; ii < times.length-1; ii++) {
+        for (let index = 0; index < 10; index++) {
+            propState =  runge_kutta(propState, (times[ii+1] - times[ii]) / 10)
+            propTime += (times[ii+1] - times[ii]) / 10
+        }
+        let obs = checkSensors(propState, propTime, {sensors: [mainWindow.satellites[sat].obs[ii].sensor], mask: false})
+        outObs.push(...obs)
     }
     return outObs
 }
@@ -214,7 +273,7 @@ function Coe2PosVelObject(coe = {a: 42164.1401, e: 0, i: 0, raan: 0, arg: 0, tA:
 }
 
 function convertObsToVector(obs) {
-    obs = obs.map(ob => ob.data)
+    obs = obs.map(ob => ob.obs)
     return math.reshape(obs, [1,-1])[0]
 }
 
@@ -233,6 +292,7 @@ function createJacobian(sat, state) {
         let delState = state.slice()
         delState[index] += index < 3 ? 0.1 : 0.0001
         let delObsVector = convertObsToVector(copyObHistory(sat, delState))
+
         jac.push(math.dotDivide(math.subtract(delObsVector, baseObsVector), index < 3 ? 0.1 : 0.0001))
     }
     jac = math.transpose(jac)
@@ -264,4 +324,26 @@ function choleskyDecomposition(matrix = [[25, 15, -5],[15, 18,  0],[-5,  0, 11]]
         }
     }
     return a
+}
+
+function lineSphereIntercetionBool(line = [-0.45, 0, 0.45], lineOrigin = [282.75,0,0], sphereOrigin = [0,0,0], sphereRadius=200) {
+    line = math.dotDivide(line, math.norm(line))
+    let check = math.dot(line, math.subtract(lineOrigin, sphereOrigin)) ** 2 - (math.norm(math.subtract(lineOrigin, sphereOrigin)) ** 2 - sphereRadius ** 2)
+    return check > 0
+}   
+
+function importState(t) {
+    t = t.value.split(/ {2,}/)
+    let time = t[0]
+    let position = t.slice(1,7).map(n => Number(n))
+    let sun = t.slice(8,11).map(n => Number(n))
+    let long = Number(t[7])
+    let apparentLong = math.atan2(position[1], position[0]) * 180 / Math.PI
+    mainWindow.satellites= [{
+        origState: position,
+        obFreq: 900,
+        obs: []
+    }]
+    mainWindow.initSun = sun
+    mainWindow.planet.sidAngle = (apparentLong - long) * Math.PI / 180
 }
