2ebc0b5016ab31e98084f56a68a30f2bc2bcc461
pva <pvanza12@gmail.com>
2022-12-11 22:37:48 -0700

    recursive update FTW

--- views/js/propTest.js
+++ views/js/propTest.js
@@ -1,8 +1,24 @@
-let state1 = '2000-01-01T11:59:28.000   6.899999999999998e+03   0.000000000000000e+00   0.000000000000000e+00   0.000000000000000e+00  -1.057790461084520e+00   7.526570219427628e+00'.split(/ +/)
+let c = [
+    null,
+    null,
+    [-1.08262617385222E-03,-2.66739475237484E-10, 1.57461532572292E-06],
+    [2.53241051856772E-06,2.19314963131333E-06, 3.09043900391649E-07, 1.00583513408823E-07],
+    [0.16193312050719E-05,-0.50872530365024E-06, 0.78412230752366E-07, 0.59215743214072E-07, -0.39823957404129E-08]
+]
+let s = [
+    null,
+    null,
+    [0,1.78727064852404E-09, -9.03872789196567E-07],
+    [0,2.68087089400898E-07, -2.11430620933483E-07, 1.97221581835718E-07],
+    [0,-0.44945993508117E-06, 0.14815545694714E-06, -0.12011291831397E-07, 0.65256058113396E-08]
+]
+let state1 = '2000-01-01T11:59:28.000   4.216400000000000e+04   0.000000000000000e+00   0.000000000000000e+00   0.000000000000000e+00   3.074666282970636e+00   0.000000000000000e+00'.split(/ +/)
+// let state1 = '2000-01-01T11:59:28.000   6.899999999999998e+03   0.000000000000000e+00   0.000000000000000e+00   0.000000000000000e+00  -1.057790461084520e+00   7.526570219427628e+00'.split(/ +/)
 let state1Epoch = new Date(state1.shift())
 state1 = state1.map(s => Number(s))
 
-let state2 = '2000-01-01T15:57:35.427  -6.887697422082107e+03   1.893504337639188e+01  -2.837309607148014e+02   3.130172226858626e-01   1.059259368246158e+00  -7.527086168008529e+00'.split(/ +/)
+let state2 = '2000-01-02T11:59:28.000   4.215742207079439e+04   7.462398964675152e+02  -2.699404004486305e-04  -5.441892521812076e-02   3.074183715447266e+00   1.788172555132985e-08'.split(/ +/)
+// let state2 = '2000-01-02T11:59:28.000   3.779791107132051e+03  -7.356672460036997e+02   5.721440326077074e+03  -6.356547421960440e+00  -6.937474159781969e-01   4.102307469853048e+00'.split(/ +/)
 let state2Epoch = new Date(state2.shift())
 state2 = state2.map(s => Number(s))
 let state12 = '2000-01-01T11:59:28.000   4.215757821857408e+04   7.357511894821255e+02   1.284258478342382e+01  -5.366032562941529e-02   3.073729781158194e+00   5.365215290191231e-02'.split(/ +/)
@@ -23,32 +39,25 @@ let tbstate1 = propToTimeJ2(state1, timeDelta, false)
 console.log(math.norm(math.subtract(state2, propToTimeJ2(state1, timeDelta, false))));
 console.log('J2 Analytic');
 console.log(math.norm(math.subtract(state2, propToTimeJ2(state1, timeDelta))));
+console.log('Higher Analytic');
+console.log(math.norm(math.subtract(state2, higherAnalytic(state1, timeDelta))));
 console.log('J2 Numeric');
 console.log(math.norm(math.subtract(state2, propToTimeJ2Numeric(state1, timeDelta))));
 console.log('HP Numeric');
 let j4state1 = propToTimeHPNumeric(state1, timeDelta, state1Epoch)
 console.log(math.norm(math.subtract(state2, j4state1)));
-// console.log('Actual relative position');
-// let actRpo = math.subtract(state2, state22)
-// console.log(actRpo);
-// console.log('Calculated relative position');
-// let j4Rpo = math.subtract(j4state1, j4state12)
-// console.log(j4Rpo, math.norm(math.subtract(j4Rpo, actRpo)));
-// let tbRpo = math.subtract(tbstate1, tbstate12)
-// console.log('Calculated relative position TB');
-// console.log(tbRpo, math.norm(math.subtract(tbRpo, actRpo)));
 
 
 function propToTimeJ2(state = [42164.14, 0, 0, 0, 3.0746611796284924, 0], dt = 86164, j2 = true) {
     state = PosVel2CoeNew(state.slice(0,3), state.slice(3,6))
     if (j2) {
         state.tA = propTrueAnomalyj2(state.tA, state.a, state.e, state.i, dt)
-        let j2 = 1.082626668e-3
+        let j2 = -1.082626668e-3
         let n = (398600.4418 / state.a / state.a / state.a) ** 0.5
         let rEarth = 6378.1363
         let p = state.a * (1 - state.e ** 2)
         let raanJ2Rate = -3*n*rEarth*rEarth*j2*Math.cos(state.i) / 2 / p / p
-        let argJ2Rate = 3 * n * rEarth * rEarth * j2 * (4 - 5 * Math.sin(state.i) ** 2) / 4 / p / p 
+        let argJ2Rate = 3 * n * rEarth * rEarth * j2 * (4 - 5 * Math.sin(state.i) ** 2) / 4 / p / p
         state.raan += raanJ2Rate * dt
         state.arg += argJ2Rate * dt
     }
@@ -69,7 +78,7 @@ function propToTimeJ2Numeric(state = [42164.14, 0, 0, 0, 3.0746611796284924, 0],
     return state
 }
 function propToTimeHPNumeric(state = [42164.14, 0, 0, 0, 3.0746611796284924, 0], dt = 86164, startEpoch) {
-    let t = 0, step = 20
+    let t = 0, step = 250
 
     while (t < (dt-step)) {
         state = runge_kuttaHP(state, step, new Date(startEpoch - (-t * 1000)))
@@ -250,8 +259,7 @@ function Coe2PosVelObject(coe = {a: 42164.1401, e: 0, i: 0, raan: 0, arg: 0, tA:
         vz: state[1][2][0]
     };
 }
-
-function twoBodyJ2(position = [42164, 0, 0, 0, 3.074, 0]) {
+function twoBodyJ2(position = state1) {
     let mu = 398600.44189, j2 = 0.00108262668, re = 6378, x = position[0], y = position[1], z = position[2]
     let r = math.norm(position.slice(0,3))
     return [
@@ -269,7 +277,6 @@ function runge_kuttaj2(state = [42164, 0, 0, 0, -3.070, 0],dt) {
     let k4 = eom(math.add(state, math.dotMultiply(dt/1, k3)));
     return math.squeeze(math.add(state, math.dotMultiply(dt / 6, (math.add(k1, math.dotMultiply(2, k2), math.dotMultiply(2, k3), k4)))));
 }
-
 function twoBodyJ3(position = [42164, 0, 0, 0, 3.074, 0]) {
     let mu = 398600.44189, j2 = 0.00108262668, j3 = -0.0000025323, j4=-0.0000016204, re = 6378.1363, x = position[0], y = position[1], z = position[2]
     let r = math.norm(position.slice(0,3))
@@ -328,9 +335,6 @@ function runge_kuttaHP(state = [42164, 0, 0, 0, -3.070, 0], dt, date = new Date(
     let k4 = eom(math.add(state, math.dotMultiply(dt/1, k3)), new Date(date - (-dt * 1000)));
     return math.squeeze(math.add(state, math.dotMultiply(dt / 6, (math.add(k1, math.dotMultiply(2, k2), math.dotMultiply(2, k3), k4)))));
 }
-
-
-
 function fk5ReductionTranspose(r=[-1033.479383, 7901.2952754, 6380.3565958], date=new Date(2004, 3, 6, 7, 51, 28, 386)) {
     // Based on Vallado "Fundamentals of Astrodyanmics and Applications" algorithm 24, p. 228 4th edition
     // ECI to ECEF
@@ -350,12 +354,10 @@ function fk5ReductionTranspose(r=[-1033.479383, 7901.2952754, 6380.3565958], dat
     let lat = math.atan2(r[2], math.norm(r.slice(0,2)))
     return {lat, long}
 }
-
 function siderealTime(jdUti=2448855.009722) {
     let tUti = (jdUti - 2451545) / 36525
     return ((67310.548 + (876600*3600 + 8640184.812866)*tUti + 0.093104*tUti*tUti - 6.2e-6*tUti*tUti*tUti) % 86400)/240
 }
-
 function rotationMatrices(angle = 0, axis = 1, type = 'deg') {
     if (type === 'deg') {
         angle *= Math.PI / 180;
@@ -382,44 +384,26 @@ function rotationMatrices(angle = 0, axis = 1, type = 'deg') {
     }
     return rotMat;
 }
-
 function julianDate(yr=1996, mo=10, d=26, h=14, min=20, s=0) {
     return 367 * yr - Math.floor(7*(yr+Math.floor((mo+9)/12)) / 4) + Math.floor(275*mo/9) + d + 1721013.5 + ((s/60+min)/60+h)/24
 }
-
 function highPrecisionProp(position = [42164, 0, 0, 0, 3.074, 0], date = new Date()) {
     let mu = 398600.44189, j2 = 0.00108262668, j3 = -0.0000025323, j4=-0.0000016204, re = 6378.1363, x = position[0], y = position[1], z = position[2]
     let r = math.norm(position.slice(0,3))
-    let {lat, long} = fk5ReductionTranspose(position.slice(0,3), date)
+    // let {lat, long} = fk5ReductionTranspose(position.slice(0,3), date)
     // console.log(date, long*180/Math.PI);
-    let tbvec = [
-        x,
-        y,
-        z
+    let a = [
+        -mu* x / r ** 3,
+        -mu* y / r ** 3,
+        -mu* z / r ** 3
     ]
-    let j2vec = [
-        x*(-3*j2*re*re*(3*Math.sin(lat)**2 - 1) / 2 / r / r - z / r / r /((x*x+y*y) ** 0.5) * 3*j2*re*re*Math.cos(lat)*Math.sin(lat)),
-        y*(-3*j2*re*re*(3*Math.sin(lat)**2 - 1) / 2 / r / r - z / r / r /((x*x+y*y) ** 0.5) * 3*j2*re*re*Math.cos(lat)*Math.sin(lat)),
-        (3*j2*re**2*(2*Math.sin(2*lat)*(x*x+y*y)**0.5+(3*Math.cos(2*lat)-1)*z))/(4*r**2)
-    ]
-    let j3vec = [
-        - 2.5 * j3 * x*re ** 3 / r ** 4 * (3 * z - 7 * z ** 3 / r / r),
-        - 2.5 * j3 * y*re ** 3 / r ** 4 * (3 * z - 7 * z ** 3 / r / r),
-        - 2.5 * j3 * re ** 3 / r ** 4 * (6 * z * z- 7 * z ** 4 / r / r - 0.6 * r * r)
-    ]
-    let c21vec = c21vector(lat, long, r, re, x,y,z)
-    let c22vec = c22vector(lat, long, r, re, x,y,z)
-    let c31vec = c31vector(lat, long, r, re, x,y,z)
-    let dVec = math.add(tbvec,j2vec, c21vec, c22vec, c31vec)
-    dVec = dVec.map((s, ii) => [
-        -mu / r ** 3,
-        -mu / r ** 3,
-        -mu / r ** 3
-    ][ii]*s)
+    for (let index = 2; index <= 4; index++) {
+        let a_pert = recursiveGeoPotential(position, index, date)
+        a = math.add(a_pert.a,a)
+    }
     return [
-        position[3], position[4], position[5],...dVec]
+        position[3], position[4], position[5],...a]
 }
-
 function c21vector(lat, long, r, re,x,y,z) {
     let c = -0.241400052222093e-9, s = 0.1543099973784379e-8
     du_dr = -9*Math.sin(lat)*Math.cos(lat)*(c*Math.cos(long) + s*Math.sin(long)) * re**2 / r
@@ -432,7 +416,6 @@ function c21vector(lat, long, r, re,x,y,z) {
         1/r * du_dr * z + (x*x+y*y)**0.5 / r / r * du_dlong
     ].map(s => -s)
 }
-
 function c22vector(lat, long, r, re,x,y,z) {
     let c = 0.1574421758350994e-5, s = -0.90376666696168745e-6
     du_dr = -9*(Math.cos(lat)**2)*(c*Math.cos(2*long) + s*Math.sin(2*long)) * re**2 / r
@@ -445,7 +428,6 @@ function c22vector(lat, long, r, re,x,y,z) {
         1/r * du_dr * z + (x*x+y*y)**0.5 / r / r * du_dlat
     ].map(s => -s)
 }
-
 function c31vector(lat, long, r, re,x,y,z) {
     let c = 0.2190922081404716e-5, s = 0.2687418863136855e-6
     du_dr = -2*Math.cos(lat)*(15*Math.sin(lat)**2 - 3)*(c*Math.cos(long) + s*Math.sin(long)) * re ** 3 / r / r
@@ -482,3 +464,106 @@ function c33vector(lat, long, r, re,x,y,z) {
         1/r * du_dr * z + (x*x+y*y)**0.5 / r / r * du_dlong
     ]
 }
+
+function higherAnalytic(state = [42164.14, 0, 0, 0, 3.0746611796284924, 0], dt = 86164) {
+    function True2Eccentric(e, ta) {
+        return Math.atan(Math.sqrt((1 - e) / (1 + e)) * Math.tan(ta / 2)) * 2;
+    }
+    function Eccentric2True(e,E) {
+        return Math.atan(Math.sqrt((1+e)/(1-e))*Math.tan(E/2))*2;
+    }
+    function solveKeplersEquation(M,e) {
+        let E = M;
+        let del = 1;
+        while (Math.abs(del) > 1e-6) {
+            del = (E-e*Math.sin(E)-M)/(1-e*Math.cos(E));
+            E -= del;
+        }
+        return E;
+    }
+    
+    let r = math.norm(state.slice(0,3))
+    let coes = PosVel2CoeNew(state.slice(0,3), state.slice(3,6)), j2 = -0.1082626925638815e-2, j4=0.162042999e-5, j6=0.5408436161399631e-6, re = 6378.1363
+    let {a, e, i, raan, arg, tA} = coes
+    let n = (398600.4418 / a**3)**0.5, p = a * (1 - e*e)
+    let dRaan = -3*j2*(re**2)*n*Math.cos(i)/2/(p**2)
+    dRaan += 3*(j2**2)*(re**4)*n*Math.cos(i)/32/(p**4) * (12 - 4*(e**2) - (80+5*(e**2))*Math.sin(i))
+    dRaan += 15*j4*(re**4)*n*Math.cos(i)/32/(p**4) * (8 + 12*(e**2) - (14+21*(e**2))*(Math.sin(i)**2))
+    dRaan += -105*j6*(re**6)*n*Math.cos(i)/1024/(p**6) * (64 + 160*e*e + 120*(e**4) - (288+720*e*e+540*(e**4))*Math.sin(i)**2 + (264+660*e*e+495*(e**4))*Math.sin(i)**4)
+    
+    let dArg = 3*n*j2*(re**2)/4/(p**2) * (4 - 5 * Math.sin(i)**2)
+    dArg += 9*n*(j2**2)*(re**4)/384/(p**4) * (56*e*e + (760-36*e*e)*(Math.sin(i)**2) - (890+45*e*e)*(Math.sin(i)**4)) 
+    dArg += -15*n*j4*(re**4)/128/(p**4) * (64 + 72*e*e - (248+252*e*e)*(Math.sin(i)**2) + (196+189*e*e)*(Math.sin(i)**4)) 
+    dArg += 105*n*j6*(re**6)/2048/(p**6) * (256 + 960*e*e + 320*(e**4) - (2048+6880*e*e+2160*(e**4))*(Math.sin(i)**2) - (4128+13080*e*e+3960*(e**4))*(Math.sin(i)**4)- (2376+14520*e*e+2145*(e**4))*(Math.sin(i)**6))
+    
+    let dM = 3*n*(re**2)*j2*((1-e*e)**0.5)/4/(p**2) * (2 - 3*Math.sin(i)**2)
+
+    let eccAnom = True2Eccentric(e, tA)
+    let meanAnom = eccAnom - e*Math.sin(eccAnom)
+    meanAnom += n*dt
+    meanAnom += dM*dt
+    eccAnom = solveKeplersEquation(meanAnom, e)
+    tA = Eccentric2True(e, eccAnom)
+
+    raan += dRaan*dt
+    arg += dArg*dt
+    
+    let del_a = j2*re*re*((a/r)**3 - 1/((1-e*e)**1.5) + (-1*(a/r)**3 + 1/((1-e*e)**1.5) + (a/r)**3*Math.cos(2*arg+2*tA))*3*(Math.sin(i)**2)/2)
+    let del_i = j2*re*re*Math.sin(2*i)/8/p/p * (3*Math.cos(2*arg+2*tA)+3*e*Math.cos(2*arg+tA)+e*Math.cos(2*arg+3*tA))
+    state = Object.values(Coe2PosVelObject({
+        a: a, e, i: i, raan, arg, tA
+    }))
+    return state
+}
+
+function timeRecursive() {
+    console.time()
+    recursiveGeoPotential()
+    console.timeEnd()
+}
+
+function recursiveGeoPotential(state = state1, k = 2, date = state1Epoch) {
+    let {lat, long} = fk5ReductionTranspose(state.slice(0,3), date)
+    let re = 6378.1363, r = math.norm(state.slice()), x = state[0], y=state[1], z=state[2]
+    let p = [[1],[Math.sin(lat), Math.cos(lat)]]
+    for (let order = 2; order <= k; order++) {
+        let row = []
+        for (let rowNum = 0; rowNum <= order; rowNum++) {
+            if (rowNum === 0) row.push(((2*order-1)*Math.sin(lat)*p[order-1][0] - (order-1)*p[order-2][0])/order)
+            else if (rowNum === order) row.push((2*order-1)*Math.cos(lat)*p[order-1][order-1])
+            else {
+                let po_2m = rowNum > (order-2) ? 0 : p[order-2][rowNum]
+                row.push(po_2m + (2*order-1)*Math.cos(lat)*p[order-1][rowNum-1])
+            }
+        }
+        p.push(row)
+    }
+    let du_dr = 0, du_dlat = 0, du_dlong = 0
+    for (let order = k; order <= k; order++) {
+        for (let m = 0; m <= order; m++) {
+            du_dr += (re/r)**order*(order+1)*p[order][m]*(c[order][m]*Math.cos(m*long)+s[order][m]*Math.sin(m*long))
+
+            let pt_m_1 = (m+1) > order ? 0 : p[order][m+1] 
+            du_dlat += (re/r)**order * (pt_m_1 - m * Math.tan(lat) * p[order][m]) * (c[order][m]*Math.cos(m*long) + s[order][m]*Math.sin(m*long))
+            du_dlong += (re/r)**order * m * p[order][m] * (s[order][m]*Math.cos(m*long) - c[order][m]*Math.sin(m*long))
+        }
+    }
+    du_dr *= -398600.4418/r/r
+
+    du_dlat *= 398600.4418/r
+
+    du_dlong *= 398600.4418/r
+
+    let a_i = (1/r * du_dr - z/r/r/Math.sqrt(x*x+y*y)*du_dlat)*x - (1/(x*x+y*y)*du_dlong)*y
+    let a_j = (1/r * du_dr - z/r/r/Math.sqrt(x*x+y*y)*du_dlat)*y + (1/(x*x+y*y)*du_dlong)*x
+    let a_k = 1/r * du_dr * z + Math.sqrt(x*x+y*y)/r/r * du_dlat
+
+    return {p, a: [a_i, a_j, a_k]}  
+}
+
+function lagrangeCalc(n,x) {
+    if (n === 0) return 1
+    if (n === 1) return x
+    return (2 * n - 1) / n * x * lagrangeCalc(n - 1, x) - (n - 1) / n * lagrangeCalc(n - 2, x)
+}
+
